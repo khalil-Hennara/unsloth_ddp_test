9f2e1b92caa07f2397af73d9d0ce981a87f92589
hennara <hennara.khalil@gmail.com>
2025-05-29 10:18:56 +0300

    test

---
+++ train_ddp_torchrun.py
@@ -0,0 +1,66 @@
+import os
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+from torch.utils.data import DataLoader, DistributedSampler
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def setup():
+    # torchrun sets these environment variables
+    local_rank = int(os.environ["LOCAL_RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+    rank = int(os.environ["RANK"])
+    dist.init_process_group("nccl")
+    torch.cuda.set_device(local_rank)
+    return local_rank, world_size, rank
+
+def cleanup():
+    dist.destroy_process_group()
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+    def __len__(self):
+        return 10000
+    def __getitem__(self, idx):
+        enc = self.tokenizer("Hello world!", return_tensors="pt", padding=True,padding_side='right', max_length=256)
+        return {
+            "input_ids": enc["input_ids"].squeeze(0),
+            "attention_mask": enc["attention_mask"].squeeze(0),
+            "labels": enc["input_ids"].squeeze(0),
+        }
+
+def train(args):
+    local_rank, world_size, rank = setup()
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    model = AutoModelForCausalLM.from_pretrained(args.model_name)
+    model.to(local_rank)
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[local_rank])
+
+    dataset = MyDataset(tokenizer)
+    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=True)
+    loader = DataLoader(dataset, batch_size=args.batch_size, sampler=sampler, num_workers=2, pin_memory=True)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+    model.train()
+    for epoch in range(args.epochs):
+        sampler.set_epoch(epoch)
+        for batch in loader:
+            batch = {key:batch[key].to(rank) for key in batch.keys()}
+            outputs = model(**batch)
+            loss = outputs.loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if rank == 0:
+                print(f"Epoch {epoch} | Loss: {loss.item()}")
+    cleanup()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default="meta-llama/Llama-2-7b-hf")
+    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--epochs', type=int, default=1)
+    args = parser.parse_args()
+    train(args)
